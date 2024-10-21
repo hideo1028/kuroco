5c936674bd13aee9ace1a98f57c390bba2812c7d
Hideo Oyabu <oyabu@nscreates.com>
2024-10-21 16:22:20 +0900

    add　APIかあら取得して表示

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  runtimeConfig: {
+    // Public keys that are exposed to the client
+    public: {
+      apiBase: 'https://oyabu.g.kuroco.app'
+    }
+  },
 })
